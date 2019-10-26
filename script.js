8fb6b4cdf24e04e72504de8dc781a5f0866877da
Aditya Kumar <thisisaditya10@gmail.com>
2019-10-26 14:26:32 +0530

    UCD Website - Registration Form

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$('.btn-faculty').click(function() {
-  $('.student').addClass('remove-section');
+$('.btn-faculty').click(function () {
+	$('.student').addClass('remove-section');
 	$('.faculty').removeClass('active-section');
-  $('.btn-faculty').removeClass('active');
-  $('.btn-student').addClass('active');
+	$('.btn-faculty').removeClass('active');
+	$('.btn-student').addClass('active');
 });
 
-$('.btn-student').click(function() {
-  $('.student').removeClass('remove-section');
-	$('.faculty').addClass('active-section');	
-  $('.btn-faculty').addClass('active');
-  $('.btn-student').removeClass('active');
+$('.btn-student').click(function () {
+	$('.student').removeClass('remove-section');
+	$('.faculty').addClass('active-section');
+	$('.btn-faculty').addClass('active');
+	$('.btn-student').removeClass('active');
 });
